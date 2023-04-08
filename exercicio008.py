31d7c75c679f512edd094562f6140483d5d7e1a9
danielly paulino <98064933+daniellycodes@users.noreply.github.com>
2023-04-08 20:16:19 -0300

    Conversão de medida de distância

---
+++ exercicio008.py
@@ -0,0 +1,13 @@
+m = float(input('Digite uma distância em metros: '))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+print('A medida de {}m equivale a {}km'.format(m, km))
+print('A medida de {}m equivale a {}hm'.format(m, hm))
+print('A medida de {}m equivale a {}dam'.format(m, dam))
+print('A medida de {}m equivale a {}dm'.format(m, dm))
+print('A medida de {}m equivale a {}cm'.format(m, cm))
+print('A medida de {}m equivale a {}mm'.format(m, mm))
