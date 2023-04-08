0288167a339e97f1736fdd85b2163f7907b7c343
danielly paulino <98064933+daniellycodes@users.noreply.github.com>
2023-04-08 20:14:02 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ exercicio006.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = pow(numero, (1/2))
+print('O número é {}, seu dobro é {}, seu triplo é {} e a raiz quadrada é {}'.format(numero, dobro, triplo, round(raiz,2)))
